ef7d18a35612492d02f6206e3c387f952d449988
shainova <shainova@gmail.com>
2018-05-22 14:39:01 -0500

    Created functions to streamline data extraction and plotting

--- get_data_sqlite.R
+++ get_data_sqlite.R
@@ -1,4 +1,3 @@
-library(sqldf)
 library(tidyverse)
 
 
@@ -37,94 +36,113 @@ get_biting_rate <- function(parameter_file, sampling_period=30){
   sapply(BITING_RATE, mean)
 }
 
+setwd('~/Documents/malaria_interventions_sqlite/')
+get_data <- function(parameter_space, scenario, experiment, run, sampling_period=30){
+  require(sqldf)
+  # Initialize
+  base_name <- paste('PS',parameter_space,'_',scenario,'_E',experiment,'_R',run,sep='')
+  sqlite_file <- paste(base_name,'.sqlite',sep='')
+  # parameter_file <- paste(base_name,'.py',sep='') # This may be necessary so I leave it
+  
+  # Extract data from sqlite. variable names correspond to table names
+  db <- dbConnect(SQLite(), dbname = sqlite_file)
+  summary_general <- dbGetQuery(db, 'SELECT * FROM summary')
+  sampled_infections <- dbGetQuery(db, 'SELECT * FROM sampled_infections')
+  summary_general$PS <- parameter_space
+  summary_general$exp <- experiment
+  summary_general$scenario <- scenario
+  summary_general$run <- run
+  summary_general$year <- gl(n = max(summary_general$time)/360, length = nrow(summary_general), k = 1)
+  summary_general$month <- gl(n = 12, k = 1, length = nrow(summary_general),labels = c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'), ordered = F)
+  
+  # Extract statistics
+  if (nrow(summary_general)%%sampling_period==1){# The beginning of the data set in E00 has a timestap of 0 and this line is unnecesary. So remove it
+    summary_general <- summary_general[-1,]
+  }
 
-# Arguments ---------------------------------------------------------------
-setwd('~/Documents/malaria_interventions_sqlite')
-# setwd('~/GitHub/')
-parameter_space <- '03'
-scenario <- 'N'
-experiment <- '02' # 00 is for the checkpoint and control
-run <- 1
-base_name <- paste('PS',parameter_space,'_',scenario,'_E',experiment,'_R',run,sep='')
-sqlite_file <- paste(base_name,'.sqlite',sep='')
-parameter_file <- paste(base_name,'.py',sep='')
+  ## Prevalence
+  summary_general$prevalence <- summary_general$n_infected/10^4
+  
+  ## Or get EIR from the table in the sqlite
+  summary_general$EIR <- summary_general$n_infected_bites/10000 # 10000 is the size of the human population
+  
+  # ##EIR can also be calculated manually as EIR=biting_rate * prevalence
+  # biting_rate <- get_biting_rate(parameter_file)
+  # summary_general$b <- NA
+  # summary_general$b[] <- biting_rate # The [] is for recycling the biting_rate
+  # summary_general$EIR1 <- summary_general$prevalence*summary_general$b*sampling_period
+  
+  # # Annual EIR is given by dividing by the sampling period to get a daily EIR, then multiplying by 360
+  # summary_general %>% mutate(eir_y=EIR2/sampling_period*360) %>% group_by(year) %>% summarise(EIR_Year=mean(eir_y))
 
-# Extract from sqlite -----------------------------------------------------
-db <- dbConnect(SQLite(), dbname = sqlite_file)
-# sampled_hosts <- dbGetQuery(db, 'SELECT * FROM sampled_hosts')
-summary_general <- dbGetQuery(db, 'SELECT * FROM summary')
-sampled_infections <- dbGetQuery(db, 'SELECT * FROM sampled_infections')
-# summary_general <- inner_join(sampled_hosts, summary_general)
-
-# Main code ---------------------------------------------------------------
-if (nrow(summary_general)%%30==1){# The beginning of the data set has a timestap of 0 and this line is unnecesary. So remove it
-  summary_general <- summary_general[-1,]
+  # MOI#
+  meanMOI <- sampled_infections %>% group_by(time, host_id) %>% summarise(MOI=length(strain_id)) %>% group_by(time) %>% summarise(meanMOI=mean(MOI))
+  summary_general <- inner_join(summary_general, meanMOI) # Add MOI to the summary
+  
+  # Host age structure
+  hosts <- dbGetQuery(db, 'SELECT * FROM hosts')
+  names(hosts)[1] <- 'host_id'
+  hosts$lifespan <- round((hosts$death_time-hosts$birth_time)/30)
+  hosts <- subset(hosts, host_id%in%sampled_infections$host_id)
+  sampled_infections <- left_join(sampled_infections, hosts, by='host_id')
+  sampled_infections$host_age <- round((sampled_infections$time-sampled_infections$birth_time)/30)
+  
+  return(list(summary_general=summary_general, sampled_infections=sampled_infections))
 }
 
-# Prevalence
-summary_general$prevalence <- summary_general$n_infected/10^4
+# This function uses the list obtained by get_data() and generates relevant plots
+generate_plots <- function(data, time_range=NULL){
+  data1 <- data[[1]]
+  data2 <- data[[2]]
+  if (is.null(time_range)){
+    time_range <- c(min(data1$time), max(data1$time))
+  }
+  plot_variables <- data1 %>% 
+    select(-n_infected, -year, -month, -PS, -exp, -run, -scenario) %>% 
+    filter(time>time_range[1]&time<time_range[2]) %>%    gather(variable, value, -time) %>% 
+    ggplot(aes(time, value, color=variable))+
+    geom_line()+
+    facet_wrap(~variable, scales = 'free')+
+    mytheme+theme(legend.position = 'none')
+  
+  plot_eir <- data1 %>% 
+    ggplot(aes(x=month,y=EIR))+
+    geom_boxplot()+
+    geom_point(stat='summary', fun.y=mean, color='red')+
+    stat_summary(fun.y=mean, geom="line")+mytheme
+  
+  # Plot the age structure of infected hosts
+  plot_age_structure <- data2 %>% group_by()%>% ggplot(aes(x=host_age))+geom_histogram() + 
+    labs(x='Infected host age (months)') + 
+    geom_vline(xintercept = 60) +
+    mytheme
+  return(list(plot_variables, plot_eir, plot_age_structure))
+}
 
-#EIR
-#EIR=biting_rate * prevalence
-biting_rate <- get_biting_rate(parameter_file)
-summary_general$b <- NA
-summary_general$b[] <- biting_rate # The [] is for recycling the biting_rate
-summary_general$EIR <- summary_general$prevalence*summary_general$b*30
 
-# MOI
-meanMOI <- sampled_infections %>% group_by(time, host_id) %>% summarise(MOI=length(strain_id)) %>% group_by(time) %>% summarise(meanMOI=mean(MOI))
-summary_general <- inner_join(summary_general, meanMOI)
+PS03_S_00 <- get_data(parameter_space = '03', scenario = 'S', experiment = '00', run = 1)
+PS03_S_01 <- get_data(parameter_space = '03', scenario = 'S', experiment = '01', run = 1)
+PS03_S_02 <- get_data(parameter_space = '03', scenario = 'S', experiment = '02', run = 1)
+PS03_S_03 <- get_data(parameter_space = '03', scenario = 'S', experiment = '03', run = 1)
 
-# Host age structure
-hosts <- dbGetQuery(db, 'SELECT * FROM hosts')
-names(hosts)[1] <- 'host_id'
-hosts$lifespan <- round((hosts$death_time-hosts$birth_time)/30)
-hosts <- subset(hosts, host_id%in%sampled_infections$host_id)
-sampled_infections <- left_join(sampled_infections, hosts, by='host_id')
-sampled_infections$host_age <- round((sampled_infections$time-sampled_infections$birth_time)/30)
+PS03_N_00 <- get_data(parameter_space = '03', scenario = 'N', experiment = '00', run = 1)
+PS03_N_01 <- get_data(parameter_space = '03', scenario = 'N', experiment = '01', run = 1)
+PS03_N_02 <- get_data(parameter_space = '03', scenario = 'N', experiment = '02', run = 1)
+PS03_N_03 <- get_data(parameter_space = '03', scenario = 'N', experiment = '03', run = 1)
 
-sampled_infections %>% ggplot(aes(x=host_age))+geom_histogram() + labs(x='Infected host age (months)') # Plot the age structure of infected hosts
-
-summary_general %>% 
-  select(-n_infected) %>% 
-  # filter(time>10000) %>%
-  gather(variable, value, -time) %>% 
-  ggplot(aes(time, value, color=variable))+
-  geom_line()+
-  facet_wrap(~variable, scales = 'free')+
-  mytheme+theme(legend.position = 'none')
-
-# Annual biting rate is given by taking an average over 12 months and multiplying by 30.
-summary_general$year <- gl(n = max(summary_general$time)/360, length = nrow(summary_general), k = 1)
-summary_general %>% group_by(year) %>% summarise(eir_y=mean(EIR)*30)
-
-summary_general$month <- gl(n = 12, k = 1, length = nrow(summary_general),labels = c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'), ordered = F)
-summary_general %>% 
-  ggplot(aes(x=month,y=EIR))+
-  geom_boxplot()+
-  geom_point(stat='summary', fun.y=mean, color='red')+
-  stat_summary(fun.y=mean, geom="line")+mytheme
-
-# Create an object with the results
-summary_general$PS <- parameter_space
-summary_general$exp <- experiment
-summary_general$scenario <- scenario
-summary_general$run <- run
-assign(paste('results_',base_name,sep=''), summary_general)
+plots <- generate_plots(PS03_N_02)
+plots[[1]]  
 
 # Compare between experiments ---------------------------------------------
 
-d <- rbind(results_PS03_S_E01_R1,
-           # results_PS03_S_E02_R1,
-           results_PS03_S_E03_R1,
-           # results_PS03_S_E04_R1)
-           results_PS03_S_E05_R1)
+d <- rbind(PS03_S_01[[1]],
+           PS03_S_02[[1]],
+           PS03_S_03[[1]])
 
-d <- rbind(results_PS03_N_E00_R1,results_PS03_N_E01_R1,results_PS03_N_E02_R1)
 # mintime=d %>% group_by(exp) %>% summarise(m=max(time)) %>% summarise(min(m))
 # mintime=mintime[1,1]
 # pdf('seasonal_comparison.pdf',16,10)
-time_range <- c(10800,14400)
+time_range <- c(30000,36000)
 d %>%
   select(-year, -month, -n_infected) %>% 
   filter(time>time_range[1]&time<time_range[2]) %>%
@@ -138,7 +156,7 @@ d %>%
   ggplot(aes(x=month,y=EIR, color=exp, group=exp))+
   # geom_boxplot()+
   geom_point(stat='summary', fun.y=mean)+
-  scale_y_continuous(limits = c(0,10))+
+  # scale_y_continuous(limits = c(0,10))+
   stat_summary(fun.y=mean, geom="line")+
   mytheme
 dev.off()
