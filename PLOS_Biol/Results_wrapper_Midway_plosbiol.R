28bffc0e653c3221bb0a533dcdc48cbf34ba1e02
Shai Pilosof <shainova@gmail.com>
2019-01-21 18:06:27 -0600

    small fixes

--- PLOS_Biol/Results_wrapper_Midway_plosbiol.R
+++ PLOS_Biol/Results_wrapper_Midway_plosbiol.R
@@ -10,7 +10,7 @@ library(utils, quietly = T, warn.conflicts = F)
 
 
 if (length(commandArgs(trailingOnly=TRUE))==0) {
-  args <- c()
+  args <- c('S','001')
 } else {
   args <- commandArgs(trailingOnly=TRUE)
 }
@@ -19,7 +19,7 @@ PS <- str_pad(PS, 2, 'left', '0')
 scenario <- as.character(args[1]) 
 exp <- as.character(args[2]) 
 
-print(paste(PS,scenario,exp,' | '))
+print(paste(PS,scenario,exp,sep=' | '))
 
 experiments <- as.tibble(expand.grid(PS=PS,
                            scenario=scenario, 
@@ -27,13 +27,9 @@ experiments <- as.tibble(expand.grid(PS=PS,
                            run=1:50,
                            stringsAsFactors = F))
 cutoff_df <- tibble(PS=sprintf('%0.2d', c(4:6,18)),
-                    cutoff_prob=c(0.3,0.6,0.85,0.85),
-                    folder=c(
-                      '/scratch/midway2/pilosofs/PLOS_Biol/Results/cutoff_to_use/',
-                      '/scratch/midway2/pilosofs/PLOS_Biol/Results/cutoff_to_use/',
-                      '/scratch/midway2/pilosofs/PLOS_Biol/Results/cutoff_to_use/',
-                      paste('/scratch/midway2/pilosofs/PLOS_Biol/Results/18_',scenario,'/',sep='')
-                    ))
+                    cutoff_prob=c(0.3,0.6,0.85,0.85)
+                    )
+cutoff_df$folder <- paste('/scratch/midway2/pilosofs/PLOS_Biol/Results/',cutoff_df$PS,'_',scenario,'/',sep='')
 print(cutoff_df)
 
 experiments <- experiments %>% left_join(cutoff_df)
@@ -43,9 +39,10 @@ print(experiments)
 module_results <- c()
 for (i in 1:nrow(experiments)){
   run <- experiments$run[i]
+  exp <- experiments$exp[i]
   cutoff_prob <- experiments$cutoff_prob[i]
   folder <- experiments$folder[i]
-  x <- get_modularity_results(PS,scenario,run,cutoff_prob,folder = folder)
+  x <- get_modularity_results(PS = PS,scenario = scenario,exp = exp,run = run,cutoff_prob = cutoff_prob,folder = folder)
   module_results <- rbind(module_results,x)
 }
 module_results$strain_cluster <- as.integer(module_results$strain_cluster)
@@ -76,8 +73,9 @@ temporal_diversity <- c()
 for (i in 1:nrow(experiments)){
   run <- experiments$run[i]
   cutoff_prob <- experiments$cutoff_prob[i]
+  exp <- experiments$exp[i]
   folder <- experiments$folder[i]
-  x <- get_temporal_diversity(PS,scenario,run,cutoff_prob,folder = folder)
+  x <- get_temporal_diversity(PS = PS,scenario = scenario,exp = exp,run = run,cutoff_prob = cutoff_prob,folder = folder)
   temporal_diversity <- rbind(temporal_diversity,x)
 }
 temporal_diversity <- as.tibble(temporal_diversity)
@@ -87,9 +85,10 @@ write_csv(temporal_diversity, paste('/scratch/midway2/pilosofs/PLOS_Biol/Results
 mFst <- c()
 for (i in 1:nrow(experiments)){
   run <- experiments$run[i]
+  exp <- experiments$exp[i]
   cutoff_prob <- experiments$cutoff_prob[i]
   folder <- experiments$folder[i]
-  x <- get_mFst(PS,scenario,run,cutoff_prob,folder = folder)
+  x <- get_mFst(PS = PS,scenario = scenario,exp = exp,run = run,cutoff_prob = cutoff_prob,folder = folder)
   mFst <- rbind(mFst,x)
 }
 mFst <- as.tibble(mFst) 
